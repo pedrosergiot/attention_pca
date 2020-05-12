b34bff417f1034c116b5f27ab8ff0b69743e0ed6
Pedro Silva <pedrosergiot@lps.ufrj.br>
2020-05-12 18:21:16 -0300

    pca_individual funcionando

--- container/tunning_jobs_pca_individual.py
+++ container/tunning_jobs_pca_individual.py
@@ -2,12 +2,11 @@ import os
 os.environ['KERAS_BACKEND'] = 'theano'
 
 from analysisFuncs import *
-from sklearn.model_selection import KFold
 import numpy as np
-from keras.callbacks import EarlyStopping
 from joblib import load, dump
 import argparse
 
+from keras.callbacks import EarlyStopping
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
 from sklearn.decomposition import PCA
@@ -26,7 +25,6 @@ args = parser.parse_args()
 # Loading data from .mat file
 exames, target = load_data(args.dataFile)
 
-
 # analysis parameters
 fs = 601.5  # sampling rate
 f1 = 31.13  # modulating frequency 1
@@ -40,32 +38,95 @@ order = 4
 win_size = 64
 nwin = int(exames.shape[1]/win_size)
 max_pcs = 18
+electrodes_used = (1, 4, 9, 12, 13)     # Conjunto melhor resultado da Ana
 
 num_inits = 10  # number of initializations inside each iteration of k-fold
 num_folds = 5   # number of folds for k-fold cross-validation
 
-win_results = {}    # storage for results
 
+win_results = {}    # storage for results for each number of windows of signal used
 
 # Starting analysis
 for win in range(1, nwin+1):
-    # Transforming and filtering data
-    data_transformed = []
-
+    print('win')
+    # Cut data to window size and filter it
     cut_data = exames[:, 0:win * win_size, :]
 
-    for exam_index in len(exames.shape[0]):
+    pcs_results = {}    # storage for results for each number of pcs used
 
-        exam = cut_data[exam_index, :, :]
+    # Loop for determining the number of PCs to use in PCA
+    for pcs in range(1, max_pcs+1):
+        print('pcs')
+        # Transforming and filtering data
+        transformed_data = []
 
-        standard = StandardScaler()
-        transformed_exam = standard.fit_transform(exam)
+        # Loop for applying PCA individually to each exam
+        for exam_index in range(exames.shape[0]):
+            exam = cut_data[exam_index, :, :]
 
-        for pcs in range(max_pcs):
+            # Exam data transformed to zero mean and unit variance
+            standard = StandardScaler()
+            transformed_exam = standard.fit_transform(exam)
 
+            # Applies PCA and reconstructs exam with some PCs
             pca = PCA(n_components=pcs)
-            pca.fit_transform(transformed_exam)
+            transformed_exam = pca.fit_transform(transformed_exam)
+            transformed_exam = pca.inverse_transform(transformed_exam)
+            transformed_exam = standard.inverse_transform(transformed_exam)
+
+            transformed_data.append(transformed_exam)
+
+        # Transformed data of all exams is filtered and energy is calculated
+        transformed_data = np.asarray(transformed_data, dtype=np.float32)
+
+        data32, data38 = filt_data(transformed_data[:, :, electrodes_used], fs, band / 2, order, f1, f2)
+
+        energy32 = get_energy(data32)
+        energy38 = get_energy(data38)
+
+        features = np.concatenate((energy32, energy38), axis=1)
 
 
+        # Initializes K-Fold object for cross validation
+        kf = KFold(n_splits=num_folds)
 
+        results = []
+        models = []
+        histories = []
 
+        for train_index, test_index in kf.split(features):
+
+            abs_scaler = MaxAbsScaler()
+            X_train = abs_scaler.fit_transform(features[train_index])
+            y_train = target[train_index]
+
+            X_test = abs_scaler.transform(features[test_index])
+            y_test = target[test_index]
+
+            best_result_fold = 0
+            best_model = None
+            best_history = None
+
+            for init in range(num_inits):
+
+                model = load(args.configFile)
+
+                es = EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=20, restore_best_weights=True)
+                model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+                history = model.fit(X_train, y_train, epochs=500, batch_size=50, verbose=0,
+                                    callbacks=[es], validation_data=(X_test, y_test))
+
+                result_fold = model.evaluate(X_test, y_test, verbose=0)
+
+                if result_fold[1] > best_result_fold:
+                    best_result_fold = result_fold[1]
+                    best_model = model
+                    best_history = history
+
+            results.append(np.array(best_result_fold))
+            models.append(best_model)
+            histories.append(best_history)
+
+        pcs_results[pcs] = {'results': results, 'histories': histories, 'models': models}
+    win_results[win] = pcs_results
+    dump(win_results, args.outputFile)
